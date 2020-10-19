a21c949b2e4fe56584030b1edc8cb0bb6f77a58b
Kyle Mandli <kyle.mandli@gmail.com>
2020-10-19 17:07:10 -0400

    Add my_code.c and modify README.md

--- src/README.md
+++ src/README.md
@@ -1,1 +1,3 @@
 # src
+
+The file `my_code.md` has the new code.
---
+++ src/my_code.c
@@ -0,0 +1,8 @@
+// Standard IO libraries
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    printf("Hello world!")
+}
